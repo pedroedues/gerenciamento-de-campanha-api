e5974e1fc5e933127353585a69e6b3c31aee126c
Pedro Santos <espedrosantos@gmail.com>
2023-12-15 23:09:10 -0300

    feat: Implementação de command de redirecionamento de campanha

---
+++ src/GerenciamentoDeCampanhas.Api/Application/Commands/RedirecionamentoCampanha/RedirecionamentoCampanhaCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace GerenciamentoDeCampanhas.Api.Application.Commands.RedirecionamentoCampanha
+{
+    public class RedirecionamentoCampanhaCommand : IRequest<RedirecionamentoCampanhaCommandResponse>
+    {
+        public string LinkDeAcesso { get; set; }
+
+        public RedirecionamentoCampanhaCommand(string linkDeAcesso)
+        {
+            LinkDeAcesso = linkDeAcesso;
+        }
+    }
+}
---
+++ src/GerenciamentoDeCampanhas.Api/Application/Commands/RedirecionamentoCampanha/RedirecionamentoCampanhaCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciamentoDeCampanhas.Api.Application.Commands.RedirecionamentoCampanha
+{
+    public class RedirecionamentoCampanhaCommandResponse
+    {
+        public string LinkRedirecionamentoAtual { get; set; }
+        public int CliquesRecebidosLinkAtual { get; set; }
+        public int MaximoDeCliques { get; set; }
+
+        public RedirecionamentoCampanhaCommandResponse(string linkRedirecionamentoAtual, int cliquesRecebidosLinkAtual, int maximoDeCliques)
+        {
+            LinkRedirecionamentoAtual = linkRedirecionamentoAtual;
+            CliquesRecebidosLinkAtual = cliquesRecebidosLinkAtual;
+            MaximoDeCliques = maximoDeCliques;
+        }
+    }
+}
